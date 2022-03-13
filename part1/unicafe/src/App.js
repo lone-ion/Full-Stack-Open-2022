b96bfdf35a019f95411bd3785889bad732830eb9
lone-ion <st@DESKTOP-676BBER.localdomain>
2022-03-14 00:08:04 +0200

    unicafe step2 refactored. Missing %.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,34 +6,40 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [allClicks, setAll] = useState(0)
 
+  const handleGoodClick = () => {
+    setAll(allClicks + 1)
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setAll(allClicks + 1)
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setAll(allClicks + 1)
+    setBad(bad + 1)
+  }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+
+  const Display = ({ value, text }) => <div>{text} {value}</div>
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => {
-        setAll(allClicks + 1)
-        setGood(good + 1)
-      }}>
-        good
-      </button>
-      <button onClick={() => {
-        setAll(allClicks + 1)
-        setNeutral(neutral + 1)
-      }}>
-        neutral
-      </button>
-      <button onClick={() => {
-        setAll(allClicks + 1)
-        setBad(bad + 1)
-      }}>
-        bad
-      </button>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
       <h1>statistics</h1>
-      good {good} <br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      all {allClicks}<br />
-      average {(good - bad) / (allClicks === 0 ? 1 : allClicks)}<br />
-      positive {good * 100 / (allClicks === 0 ? 1 : allClicks)} %
+      <Display value={good} text='good' />
+      <Display value={neutral} text='neutral' />
+      <Display value={bad} text='bad' />
+      <Display value={allClicks} text='all' />
+      <Display value={(good - bad) / (allClicks === 0 ? 1 : allClicks)} text='average' />
+      <Display value={good * 100 / (allClicks === 0 ? 1 : allClicks)} text='positive' />
     </div>
   )
 }
