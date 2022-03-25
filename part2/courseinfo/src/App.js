e31c5d159781ee74fe77b9d2fd0b243c87cc5d89
lone-ion <st@DESKTOP-676BBER.localdomain>
2022-03-25 18:51:17 +0200

    Project Course information step9 completed

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,50 +1,6 @@
 import Course from './components/Course'
 
-const App = () => {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+const App = ({ courses }) => {
 
   return (courses.map(courseObj => <Course key={courseObj.id} course={courseObj} />))
 }
