02e1ec3cd407f111d64c0c9e9dca0aa0a25a2fb1
lone-ion <st@DESKTOP-676BBER.localdomain>
2022-04-01 00:27:18 +0300

    Project Phonebook step5 comp

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,56 +1,76 @@
 import { useState } from "react"
+import Persons from "./components/Persons"
+import Search from "./components/Search"
+import PersonForm from "./components/PersonForm"
 
-const Person = ({ person }) => {
-  return (
-    <li>
-      {person.name}
-    </li>
-  )
-}
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState(
-    'a new person...'
-  )
+const App = ({ personsArr }) => {
+  const [persons, setPersons] = useState(personsArr)
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [charFilter, setCharFilter] = useState('')
+
+  const applyCharFilter = persons.filter(person => person.name.toLowerCase().includes(charFilter.toLowerCase()))
+  const personsToShow = charFilter === ''
+    ? persons
+    : applyCharFilter
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObj = {
-      name: newName,
-    }
+    const names = persons.map(person => person.name)
+    const exists = names.includes(newName)
 
-    setPersons(persons.concat(personObj))
-    setNewName('')
+    if (!exists) {
+      const personObj = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      }
+      setPersons(persons.concat(personObj))
+      setNewName('')
+      setNewNumber('')
+    }
+    else {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+    }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setCharFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input
-            value={newName}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />
-        )}
-      </ul>
+      <Search
+        charFilter={charFilter}
+        handleFilterChange={handleFilterChange}
+      />
+      <h2>add a new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        addPerson={addPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      {personsToShow.map(person =>
+        <Persons
+          key={person.id}
+          name={person.name}
+          number={person.number}
+        />
+      )}
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Persons = ({ name, number }) => {
+   return (
+      <div>
+         {name} {number}
+      </div>
+   )
+}
+
+export default Persons
