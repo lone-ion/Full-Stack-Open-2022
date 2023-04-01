65def16468366f9c7b0e8943fc3eb3fac583b97b
lone-ion <stilos@gmail.com>
2023-04-02 00:58:56 +0300

    step2, test verifies unique identifier is named id

--- part4/Blog-List/tests/blog_api.test.js
+++ part4/Blog-List/tests/blog_api.test.js
@@ -3,6 +3,26 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+   {
+      content: '',
+      important: false,
+   },
+   {
+      content: '',
+      important: true,
+   },
+]
+
+beforeEach(async () => {
+   await Blog.deleteMany({})
+   let blogObject = new Blog(initialBlogs[0])
+   await blogObject.save()
+   blogObject = new Blog(initialBlogs[1])
+   await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
    await api
@@ -11,10 +31,30 @@ test('blogs are returned as json', async () => {
       .expect('Content-Type', /application\/json/)
 })
 
-test('there are two blogs', async () => {
+test('all blogs are returned', async () => {
    const response = await api.get('/api/blogs')
 
-   expect(response.body).toHaveLength(2)
+   expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+
+// test('a specific note is within the returned notes', async () => {
+//    const response = await api.get('/api/notes')
+
+//    const contents = response.body.map(r => r.content)
+//    expect(contents).toContain('Browser can execute only JavaScript')
+// })
+
+
+test('unique identifier property of the blog posts is named id', async () => {
+   const response = await api.get('/api/blogs')
+
+   for (objItem of response.body) {
+      for (prop in objItem) {
+         expect(prop).toBeDefined();
+      }
+   }
+
 })
 
 afterAll(async () => {
