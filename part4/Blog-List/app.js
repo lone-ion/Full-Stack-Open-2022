0be57f6c4d2ddc466f29abee1e0c1cb460c00efc
lone-ion <stilos@gmail.com>
2023-04-15 20:25:32 +0300

    Implement create new users. Step 3

--- part4/Blog-List/app.js
+++ part4/Blog-List/app.js
@@ -4,9 +4,11 @@ const app = express()
 // const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
 
@@ -25,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
