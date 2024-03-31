11850b61aa8224e6b3543802a8fc6b20eebf1156
lone-ion <stilos@gmail.com>
2024-03-31 13:46:44 +0300

    finish ex5.15: Blog List Tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -13,7 +13,7 @@ test('renders title and author only', () => {
   render(<Blog blog={blog} />)
 
   const element = screen.getByText(
-    'Component testing is done with react-testing-library Helsinki Uni',
+    'Component testing is done with react-testing-library Helsinki Uni'
   )
 })
 
@@ -24,17 +24,41 @@ test('clicking the button returns also url and likes', async () => {
     url: 'https://fullstackopen.com',
     likes: 17,
     user: {
-      name: 'Sotiris'
-    }
+      name: 'Sotiris',
+    },
   }
 
-const { container } = render(<Blog blog={blog} />)
+  const { container } = render(<Blog blog={blog} />)
 
-const user = userEvent.setup()
-const button = screen.getByText('show')
-await user.click(button)
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
 
-const div = container.querySelector('.detailed-blog')
-expect(div).toHaveTextContent('https://fullstackopen.com')
-expect(div).toHaveTextContent('likes 17')
+  const div = container.querySelector('.detailed-blog')
+  expect(div).toHaveTextContent('https://fullstackopen.com')
+  expect(div).toHaveTextContent('likes 17')
 })
+
+test('clicking the like button twice calls the evet handler also twice'),
+  async () => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      author: 'Helsinki Uni',
+      url: 'https://fullstackopen.com',
+      likes: 17,
+      user: {
+        name: 'Sotiris',
+      },
+    }
+
+    const mockHandler = vi.fn()
+
+    render(<Note note={note} updateLikes={mockHandler} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('like')
+    await user.click(button)
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  }
