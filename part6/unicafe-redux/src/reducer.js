0c5049293eb17c92b3a3189cd626e5dedf523936
lone-ion <stilos@gmail.com>
2024-05-04 08:09:51 +0300

    finish ex-6.1: Unicafe Revisited, step 1

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  let changedState = {}
+  switch (action.type) {
+    case 'GOOD':
+      changedState = { ...state }
+      changedState.good++
+      return changedState
+    case 'OK':
+      changedState = { ...state }
+      changedState.ok++
+      return changedState
+    case 'BAD':
+      changedState = { ...state }
+      changedState.bad++
+      return changedState
+    case 'ZERO':
+      return state
+    default: return state
+  }
+  
+}
+
+export default counterReducer
