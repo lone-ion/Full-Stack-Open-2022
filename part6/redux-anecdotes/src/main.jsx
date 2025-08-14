901fc0b7ccf5219da7da1e86625036f71ae1f80b
Sotiris Tilos <stilos@gmail.com>
2025-08-14 10:54:42 +0300

    finish ex-6.10 Better Anecdotes, step 8

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,14 +1,22 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 
 import filterReducer from './reducers/filterReducer'
 import anecdoteReducer from './reducers/anecdoteReducer'
 
-const reducer = combineReducers({ anecdotes: anecdoteReducer, filter: filterReducer })
+// const reducer = combineReducers({ anecdotes: anecdoteReducer, filter: filterReducer })
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer
+  }
+})
 
-const store = createStore(reducer)
+// const store = createStore(reducer)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,33 @@
-const filterReducer = (state = 'ALL', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+// const filterReducer = (state = 'ALL', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   }
+// }
+
+// export default filterReducer
+
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: 'ALL',
+  reducers: {
+    filterChange(state, action) {
+
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+export const { filterChange } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
